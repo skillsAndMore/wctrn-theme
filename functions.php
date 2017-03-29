13298bdd6135ec3358bf46ad73beaa4e81d7141a
Andrea Barghigiani <andrea@codeat.it>
2017-03-29 11:59:45 +0200

    Edited script loading functions
    Modificate tutte le funzioni che venivano utilizzate per caricare i
    file PHP di personalizzazione in modo da permettere anche a un child
    theme di personalizzarle.

--- functions.php
+++ functions.php
@@ -123,24 +123,24 @@ add_action( 'wp_enqueue_scripts', 'wctrn_advanced_theme_scripts' );
 /**
  * Implement the Custom Header feature.
  */
-require get_template_directory() . '/inc/custom-header.php';
+require get_theme_file_path('/inc/custom-header.php');
 
 /**
  * Custom template tags for this theme.
  */
-require get_template_directory() . '/inc/template-tags.php';
+require get_theme_file_path('/inc/template-tags.php');
 
 /**
  * Custom functions that act independently of the theme templates.
  */
-require get_template_directory() . '/inc/extras.php';
+require get_theme_file_path('/inc/extras.php');
 
 /**
  * Customizer additions.
  */
-require get_template_directory() . '/inc/customizer.php';
+require get_theme_file_path('/inc/customizer.php');
 
 /**
  * Load Jetpack compatibility file.
  */
-require get_template_directory() . '/inc/jetpack.php';
+require get_theme_file_path('/inc/jetpack.php');
