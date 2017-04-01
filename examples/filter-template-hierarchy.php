6b8db82e0ff4b1dd468a398a8a8a2ec8da53e226
Andrea Barghigiani <andrea@codeat.it>
2017-04-01 18:04:22 +0200

    Added Filter Template Hierarchy
    Aggiunto file di esempio che descrive il funzionamento del filtro che
    permette di modificare il file template che viene caricato in base alla
    pagina visualizzata.

---
+++ examples/filter-template-hierarchy.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Da utilizzare come filtro in functions.php
+ *
+ * Scopri come creare e personalizzare la possibilita' di inserire il proprio logo all'interno del tuo sito permettendo al
+ * tuo cliente o all'utente che utilizzera' la piattaforma di personalizzarla aggiungendo la propria Brand Identity.
+ *
+ * @link https://developer.wordpress.org/reference/hooks/type_template_hierarchy/ Pagina di presentazione della funzionalita'
+ * @link https://make.wordpress.org/core/2016/11/03/post-type-templates-in-4-7/ Annuncio della funzionalita' in Make WordPress
+ */
+
+//Aggiungo un filtro inutile che aiuta a spiegare il funzionamento
+add_filter( 'single_template_hierarchy', function( array $templates ) {
+    array_unshift( $templates, "single-test.php" );
+    return $templates;
+} );
+
+//Filtro che mi permette di spacificare il file da caricare in base all'anno dell'archivio
+ add_filter( 'date_template_hierarchy', function( array $templates ) {
+     $year = get_query_var( 'year' );
+     array_unshift( $templates, "year-{$year}.php" );
+     return $templates;
+ } );
